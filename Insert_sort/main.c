501e54d2ad796c87c440f4f5940216c0fe5dcfb8
JackyWang2014 <339847016@qq.com>
2016-05-05 09:50:53 +0800

    Implementing

--- Insert_sort/main.c
+++ Insert_sort/main.c
@@ -9,7 +9,32 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int a[10] = {5,1,3,9,7,6,4,2,0,8};
+    
+    for (int i = 0; i<10; i++) {
+        printf("%d\n",a[i]);
+    }
+    // 降序
+    for (int i = 1; i < 10; i++) {
+        int temp = a[i];
+        for (int j = i - 1; j >= 0; j--) {
+            if (temp > a[j]) {
+                a[j+1] = a[j];
+                if (j == 0) {
+                    a[j] = temp;
+                }
+            } else {
+                    a[j+1] = temp;
+                    break;
+            }
+        }
+    }
+    
+    for (int i = 0; i<10; i++) {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
